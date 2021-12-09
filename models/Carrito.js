6c7c24ec9124796755b62d569531e5e58852b19a
Gonzalo Andrade <gonzaloandrade@MacBook-Pro-de-equipamiento.local>
2021-12-08 21:22:14 -0300

    Models Producto y Carrito

---
+++ models/Carrito.js
@@ -0,0 +1,14 @@
+class Carrito {
+    constructor() {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.codigo = codigo;
+        this.foto = foto;
+        this.precio = precio;
+        this.stock = stock;
+    }
+}
+
+module.exports = Carrito;
---
+++ models/Producto.js
@@ -0,0 +1,14 @@
+class Producto {
+    constructor() {
+        this.id = id;
+        this.timestamp = Date.now();
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.codigo = codigo;
+        this.foto = foto;
+        this.precio = precio;
+        this.stock = stock;
+    }
+}
+
+module.exports = Producto;
